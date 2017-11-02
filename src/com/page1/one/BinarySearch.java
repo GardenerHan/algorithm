549e166dcc11a76360cfda719010454838a6d5d7
GardenerHan <1129982332@qq.com>
2017-11-02 19:36:55 +0800

    algo first

---
+++ src/com/page1/one/BinarySearch.java
@@ -0,0 +1,33 @@
+package com.page1.one;
+
+import myalgorithm.basics.In;
+import myalgorithm.basics.StdIn;
+import myalgorithm.basics.StdOut;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        //args[0]是一个txt文件
+        int[] whitelist = In.readInts(args[0]) ;
+        Arrays.sort(whitelist);
+        while (!StdIn.isEmpty()){
+            int key = StdIn.readInt() - 1  ;
+            if (rank(key,whitelist) == -1){
+                StdOut.print(key);
+            }
+        }
+    }
+    public static int rank(int key,int[] a){
+        int lo = 0 ;
+        int hi = a.length -1 ;
+        while (lo <= hi){
+            int mid  = lo +(hi - lo)/2 ;
+            if (key == a[mid]) hi = mid -1 ;
+            else if (key > a[mid]) lo = mid -1 ;
+            else return mid ;
+        }
+        return  -1 ;
+
+    }
+}
---
+++ src/com/page1/one/Main.java
@@ -0,0 +1,62 @@
+package com.page1.one;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in ));
+        int total = 0 ;
+        String pername = null ;
+        int pertotal = 0 ;
+        int n = in.nextInt();
+        System.out.println("P");
+        String view[] = new String[n];
+        String person[];
+        for (int i = 0; i < n; i++) {
+            try {
+                view[i] = br.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            person = view[i].split(" ");
+            int  k = Integer.parseInt(person[1]) ;
+            int thisper = 0 ;
+            if ( k  > 90 ){
+                thisper = thisper + 2000 ;
+            }
+            if ( k > 85){
+                if (Integer.parseInt(person[2]) > 80){
+                    thisper = thisper + 4000 ;
+                }
+                if (person[4].equals("Y")){
+                    thisper = thisper + 1000 ;
+                }
+            }
+            if ( k > 80){
+                if ( Integer.parseInt(person[5]) >= 1  ){
+                    thisper = thisper + 8000 ;
+                }
+                if (person[3].equals("Y")){
+                    thisper = thisper + 850 ;
+                }
+
+            }
+            if (thisper > pertotal){
+                pername = person[0] ;
+                pertotal = thisper ;
+            }
+            total = total + thisper ;
+        }
+        in.close();
+        try {
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(pername);
+        System.out.println(pertotal);
+        System.out.print(total);
+    }
+}
